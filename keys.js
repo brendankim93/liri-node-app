75e3a4124c161f60e5af3113f3f67a94423f56b2
Brendan Kim <brendankim@Brendans-Air.attlocal.net>
2017-12-15 03:05:24 -0800

    working twitter and spotify

--- keys.js
+++ keys.js
@@ -1,16 +1,10 @@
-```JavaScript
 console.log('this is loaded');
 
-var twitterKeys = {
-  consumer_key: 'f0KEVcA9DPUjcjccfFaEBteLu',
-  consumer_secret: 'Up4h6hBnhfWu1tQO2e63WFyjpP8XSbCC6kiRHKVb5mmJVJ9wr2',
-  access_token_key: '940490339210502144-k4JA6w2N3ucXwA30ZmFDd53KQZIpneV',
-  access_token_secret: 'JY5RZ8J6qlrywdisXDYCDktP2FV1RVAX6J3C90ZOpjSWI',
+
+exports.twitterKeys = {
+	consumer_key: 'f0KEVcA9DPUjcjccfFaEBteLu',
+	consumer_secret: 'Up4h6hBnhfWu1tQO2e63WFyjpP8XSbCC6kiRHKVb5mmJVJ9wr2',
+	access_token_key: '940490339210502144-k4JA6w2N3ucXwA30ZmFDd53KQZIpneV',
+	access_token_secret: 'JY5RZ8J6qlrywdisXDYCDktP2FV1RVAX6J3C90ZOpjSWI',	
 }
-
-module.exports = twitterKeys;
-```
-
-Client ID '7c984bfcb27648fe8da3226538ed197f'
-Client Secret 'aff33ef3725d46debd44da01358021f6'
 
--- liri.js
+++ liri.js
@@ -1,15 +1,87 @@
-var Twitter = require('twitter');
+var keys = require('./keys.js');
 
-var client = new Twitter({
-  consumer_key: 'f0KEVcA9DPUjcjccfFaEBteLu',
-  consumer_secret: 'Up4h6hBnhfWu1tQO2e63WFyjpP8XSbCC6kiRHKVb5mmJVJ9wr2',
-  access_token_key: '940490339210502144-k4JA6w2N3ucXwA30ZmFDd53KQZIpneV',
-  access_token_secret: 'JY5RZ8J6qlrywdisXDYCDktP2FV1RVAX6J3C90ZOpjSWI'
+var Twitter = require("twitter");
+
+var Spotify = require('node-spotify-api');
+
+var request = require('request');
+
+var spotify = new Spotify({
+  id: '7c984bfcb27648fe8da3226538ed197f',
+  secret: 'aff33ef3725d46debd44da01358021f6'
 });
 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
+var getMyTweets = function(){
+
+	var client = new Twitter(keys.twitterKeys);
+	
+		var params = {screen_name: 'brendankim21'};
+		client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		  if (!error) {
+		    //console.log(tweets);
+		    for(var i=0; i<tweets.length; i++) {
+		    	console.log(tweets[i].created_at);
+		    	console.log(" ");
+		    	console.log(tweets[i].text);
+		    }
+		  }
+		});
+
+}
+
+var getArtistNames = function(artist) {
+	return artist.name;
+}
+
+var getMeSpotify = function(songName) {
+ 
+		spotify.search({ type: 'track', query: songName }, function(err, data) {
+		  if (err) {
+		    console.log('Error occurred: ' + err);
+		    return;
+		  }
+		 
+		 var songs = data.tracks.items;
+		 for(var i=0; i<songs.length; i++){
+		 	console.log(i);
+		 	console.log('artist(s): ' + songs[i].artists.map(getArtistNames));
+		 	console.log('song name: ' + songs[i].name);
+		 	console.log('preview song: ' + songs[i].preview_url);
+		 	console.log('album: ' + songs[i].album.name);
+		 	console.log('-----------------------------------------------------');
+		 }
+	});
+}
+
+var getMeMovie = function(movieName){
+
+		request('http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&r=json', function (error, response, body) {
+			console.log('error:', error); // Print the error if one occurred
+  			console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  			console.log('body:', body); // Print the HTML for the Google homepage.
+		})
+
+}
+
+var pick  = function(caseData, functionData) {
+	switch(caseData) {
+		case 'my-tweets' :
+			getMyTweets();
+			break;
+		case 'spotify-this-song' :
+			getMeSpotify(functionData);
+			break;
+		case 'movie-this' :
+			getMeMovie(functionData);
+			break;
+		default:
+		console.log("Liri doesn't know that");
+	}
+}
+
+var runThis = function(argOne, argTwo) {
+	pick(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]);
+
